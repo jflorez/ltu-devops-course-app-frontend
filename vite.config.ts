5b9ff1175b24c2a842e62eacb2eb1e01132f7640
Juan Florez <jflorez@planittesting.com>
2025-01-15 09:35:01 +1100

    fix dev env vars

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import * as dotenv from 'dotenv'
+import { expand } from 'dotenv-expand'
+
+// Load and expand environment variables
+const env = dotenv.config()
+expand(env)
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -16,7 +21,7 @@ export default defineConfig({
     },
   },
   define: {
-    'import.meta.env.VITE_API_BASE_URL': JSON.stringify(process.env.VITE_API_BASE_URL),
-    'import.meta.env.VITE_API_TOKEN': JSON.stringify(process.env.VITE_API_TOKEN)
+    'import.meta.env.VITE_API_BASE_URL': JSON.stringify(process.env.VITE_API_BASE_URL || ''),
+    'import.meta.env.VITE_API_TOKEN': JSON.stringify(process.env.VITE_API_TOKEN || '')
   }
 })
